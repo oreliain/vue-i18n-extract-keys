f07122dce58f7349b8b485c8889a89aaac893d64
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2019-12-27 01:43:33 +0100

    fix(cli): change defaults source and i18n patterns

--- defaults.js
+++ defaults.js
@@ -4,8 +4,8 @@ module.exports = {
     locales: ["en"],
     src: "src",
     output: path.join("i18n", "locales"),
-    sourcePatterns: ["\\.vue$", "\\.component\\.ts$"],
-    i18nPatterns: [`\\$t\\(["']([\\w-\\.]+)["'][,\\w'" ]*\\)`],
+    sourcePatterns: ["\\.vue$", "\\.ts(x)?$", "\\.js(x)?$"],
+    i18nPatterns: [`\\$t[ce]?\\(["'](.*?)["'].*?\\)`],
     verbose: false,
     withIndexFile: false,
     forceErase: false
