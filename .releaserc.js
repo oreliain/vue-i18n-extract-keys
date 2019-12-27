8f9ffd7618e8f5e848a21029c1e3ccee13a05c6d
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2019-12-27 15:47:57 +0100

    fix(ci): fix ci config

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,6 @@ module.exports = {
     branch: 'master',
     plugins: [
       '@semantic-release/commit-analyzer',
-      '@semantic-release/changelog',
       '@semantic-release/release-notes-generator',
       '@semantic-release/npm',
       '@semantic-release/github'
