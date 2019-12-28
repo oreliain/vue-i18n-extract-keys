3cd63dc4a8fe5b5089929fe355277fa2c9c526f1
Aurélien Brugalières <aurelien.brugalieres@anakeen.com>
2019-12-28 21:56:17 +0100

    fix(cli): add correct version in command

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const yargs = require("yargs");
 const options = require("./src/options");
 const Command = require("./src/Command");
+const package = require("./package.json");
 
+yargs.version(package.version);
 Object.keys(options).forEach(option => {
   yargs.option(option, options[option]);
 });
